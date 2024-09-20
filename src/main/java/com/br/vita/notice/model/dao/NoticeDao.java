7f6c62317e1d2828af719c31719241a726bc52de
HwangJaeWoon <wodns1236@naver.com>
2024-09-20 11:28:32 +0900

    remove: 오류관련 전체 삭제

--- src/main/java/com/br/vita/notice/model/dao/NoticeDao.java
+++ src/main/java/com/br/vita/notice/model/dao/NoticeDao.java
@@ -1,5 +1,23 @@
 package com.br.vita.notice.model.dao;
 
+import static com.br.vita.common.template.JDBCTemplate.close;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Properties;
+
 public class NoticeDao {
+
+	private Properties prop = new Properties();
+	
+//	public int selectNoticeListCount(Connection conn) {
+//		int listCount = 0;
+//		PreparedStatement pstmt = null;
+//		ResultSet rset = null;
+//		String sql = prop.getProperty("selectNoticeListCount");
+//		
+//		return 0;
+//	}
 
 }
--- src/main/java/com/br/vita/notice/model/service/NoticeService.java
+++ src/main/java/com/br/vita/notice/model/service/NoticeService.java
@@ -1,5 +1,35 @@
 package com.br.vita.notice.model.service;
 
+import static com.br.vita.common.template.JDBCTemplate.close;
+import static com.br.vita.common.template.JDBCTemplate.commit;
+import static com.br.vita.common.template.JDBCTemplate.getConnection;
+import static com.br.vita.common.template.JDBCTemplate.rollback;
+
+import java.sql.Connection;
+import java.util.List;
+
+import com.br.vita.notice.model.dao.NoticeDao;
+import com.br.vita.notice.model.vo.Notice;
+
 public class NoticeService {
+
+//	public int selectNoticeListCount() {
+//		
+//	private NoticeDao nDao = new NoticeDao();
+//		
+//	public int selectBoardListCount() {
+//		Connection conn = getConnection();
+//		int listCount = nDao.selectNoticeListCount(conn);
+//		close(conn);
+//		
+//		return listCount;
+//	}
+//	
+//	public List<Notice> selectBoardList(PageInfo pi){
+//		Connection conn = getConnection();
+//		List<Notice> list = nDao.selectBoardList(conn, pi);
+//		close(conn);
+//		return list;
+//	}
 
 }
