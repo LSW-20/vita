50b408a9b91d21614b3b54ce8749c1dd03fad7d0
chlqhrua <bm3871@naver.com>
2024-09-20 12:10:07 +0900

    docs MemberInfoController
    /myinfo.me
    로 서블릿 지정
    forward로 myInfo.jsp지정

---
+++ src/main/java/com/br/vita/member/controller/MemberInfoController.java
@@ -0,0 +1,43 @@
+package com.br.vita.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberInfoController
+ */
+@WebServlet("/myinfo.me")
+public class MemberInfoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberInfoController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * 응답 페이지 : /vita/views/member/myInfo.jsp
+	 * 응답 데이터 : 로그인 한 회원 데이터
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("views/member/myInfo.jsp").forward(request, response);
+	}//doGet
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
