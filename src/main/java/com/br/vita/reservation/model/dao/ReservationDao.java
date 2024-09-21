ddfed0b6cb56cbd697e9fe03ef0e6a5512b3b7e5
LIMSANGWOO <kuiki700@gmail.com>
2024-09-22 02:13:17 +0900

    chore: 기업, 사원, 예약 쿼리문 파일 추가, 예약dao 생성자 추가
    - employee, company, reservation mapper.xml 파일 추가
    - reservation dao에 prop 전역 변수와 생성자 추가

--- src/main/java/com/br/vita/reservation/model/dao/ReservationDao.java
+++ src/main/java/com/br/vita/reservation/model/dao/ReservationDao.java
@@ -1,5 +1,24 @@
 package com.br.vita.reservation.model.dao;
 
-public class ReservationDao {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class ReservationDao {
+	
+	private Properties prop = new Properties();
+	
+	public ReservationDao() {
+		
+		String filePath = ReservationDao.class.getResource("/db/mappers/company-mapper.xml").getPath();
+		
+		try {
+			prop.loadFromXML(new FileInputStream(filePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	// dao 클래스에 전역변수랑 기본생성자 내가 만들어놨어 준수양 by. 상우
 }
