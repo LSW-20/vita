e33e46ae3db10513c0a64aef022a07fa912cbe62
parkpoto0404 <parkpoto@gmail.com>
2024-09-23 15:29:02 +0900

    feat: 증명서 객체 , 진료기록 객체 생성
    헤더부분 증명서메뉴 포워딩 완료

---
+++ src/main/java/com/br/vita/issue/model/service/IssueService.java
@@ -0,0 +1,14 @@
+package com.br.vita.issue.model.service;
+
+import com.br.vita.issue.model.dao.IssueDao;
+
+public class IssueService {
+
+	
+	private IssueDao idao = new IssueDao();
+	
+	
+	
+	
+	
+}
