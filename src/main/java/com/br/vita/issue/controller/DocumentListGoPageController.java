b163bf8d9c0d1f92bdb58323fe9585a91a9f440e
parkpoto0404 <parkpoto@gmail.com>
2024-09-24 11:22:28 +0900

    feat: 마이페이지 메뉴 (증명서발급, 발급신청조회) 포워딩 완료

---
+++ src/main/java/com/br/vita/issue/controller/DocumentListGoPageController.java
@@ -0,0 +1,44 @@
+package com.br.vita.issue.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DocumentListGoPageController
+ */
+@WebServlet("/list.cr")
+public class DocumentListGoPageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DocumentListGoPageController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("/views/issue/getDocList.jsp").forward(request, response);
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
